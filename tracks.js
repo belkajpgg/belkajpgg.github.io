54e68e7660810085adff3e694c230b623700a778
belkajpgg <152399314+belkajpgg@users.noreply.github.com>
2026-03-20 01:40:13 +0200

    Add files via upload

---
+++ tracks.js
@@ -0,0 +1,16 @@
+const TRACKS = [
+  {
+    id: "timestop",
+    title: "TIMESTOP",
+    date: "20 MAR 2026",
+    cover: "covers/timestop.png",
+    links: {
+      spotify: "https://open.spotify.com/track/3BZpatFks9jivL3bsivyVV?si=28287a73b1424456",
+      appleMusic: "https://music.apple.com/ru/album/timestop-single/1881326428",
+      soundcloud: "https://soundcloud.com/5tarcry/timestop?si=bd1195e9253a481c8a540a24bc27293d",
+      youtubeMusic: "https://music.youtube.com/watch?v=-h2qNGktmNY&si=pW7g7sU9dmvqIpsm",
+      youtube: "https://youtu.be/NIMQrOFR-Cg",
+      yandex: ""
+    }
+  }
+];
