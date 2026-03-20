543e55f2a04c25e36d5a0a51fa403af293591117
belkajpgg <152399314+belkajpgg@users.noreply.github.com>
2026-03-20 02:45:52 +0200

    edit track: daaa

--- tracks.js
+++ tracks.js
@@ -6,11 +6,7 @@ const TRACKS = [
     "cover": "covers/daaa.jpg",
     "audio": "audio/daaa.mp3",
     "links": {
-      "spotify": "https://open.spotify.com/track/3BZpatFks9jivL3bsivyVV?si=28287a73b1424456",
-      "appleMusic": "https://music.apple.com/ru/album/timestop-single/1881326428",
-      "soundcloud": "https://soundcloud.com/5tarcry/timestop?si=bd1195e9253a481c8a540a24bc27293d&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
-      "youtubeMusic": "https://music.youtube.com/watch?v=-h2qNGktmNY&si=pW7g7sU9dmvqIpsm",
-      "yandex": "https://music.yandex.ru/album/40885708/track/148704353?utm_source=web&utm_medium=copy_link"
+      "youtubeMusic": "https://music.youtube.com/watch?v=TS-Pe30damc"
     }
   },
   {
