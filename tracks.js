eebceb0a57ebe33e68e8bf753fb0e69cd2fd09c5
belkajpgg <152399314+belkajpgg@users.noreply.github.com>
2026-03-20 02:33:48 +0200

    add track: daaa

--- tracks.js
+++ tracks.js
@@ -1,5 +1,19 @@
 const TRACKS = [
   {
+    "id": "daaa",
+    "title": "daaa",
+    "date": "",
+    "cover": "covers/daaa.jpg",
+    "audio": "audio/daaa.mp3",
+    "links": {
+      "spotify": "https://open.spotify.com/track/3BZpatFks9jivL3bsivyVV?si=28287a73b1424456",
+      "appleMusic": "https://music.apple.com/ru/album/timestop-single/1881326428",
+      "soundcloud": "https://soundcloud.com/5tarcry/timestop?si=bd1195e9253a481c8a540a24bc27293d&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+      "youtubeMusic": "https://music.youtube.com/watch?v=-h2qNGktmNY&si=pW7g7sU9dmvqIpsm",
+      "yandex": "https://music.yandex.ru/album/40885708/track/148704353?utm_source=web&utm_medium=copy_link"
+    }
+  },
+  {
     "id": "ooopa",
     "title": "ooopa",
     "date": "20.03.2026",
